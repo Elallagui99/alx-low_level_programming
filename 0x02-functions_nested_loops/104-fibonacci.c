163cbc588679a3110e6aa47cbd27dc4e2d153146
Elallagui99 <got.femme@gmail.com>
2023-06-22 01:18:23 +0100

    task16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long int a = 1, b = 2, c = 0;
+
+	printf("%lu, %lu", a, b);
+	for (i = 0; i < 96; i++)
+	{
+		c = a + b;
+		printf(", %lu", c);
+		a = b;
+		b = c;
+	}
+	printf("\n");
+	return (0);
+}
