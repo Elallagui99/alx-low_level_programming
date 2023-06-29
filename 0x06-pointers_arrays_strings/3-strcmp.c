a1790020f0fd6aef0ac7b1ae36745e3609f133bf
Elallagui99 <got.femme@gmail.com>
2023-06-29 20:34:41 +0100

    fix task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,18 +7,17 @@
  * Return: 0 if equal
  * >0 if first bigger, <0 if second id bigger
  */
+
 int _strcmp(char *s1, char *s2)
 {
-	int i, j = 0;
-
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		if (s1[i] == s2[i])
-			j++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	if (j == 0)
-		return (0);
-
 	return (*s1 - *s2);
-
 }
