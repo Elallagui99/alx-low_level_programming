9e89753b18b7107f3c845fe3136630ea66897f47
Elallagui99 <got.femme@gmail.com>
2023-06-29 18:56:33 +0100

    add all tasks

---
+++ 0x06-pointers_array_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes to uppercase
+ * @s: string to change
+ * Return: pointer to char
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
---
+++ 0x06-pointers_array_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encode string to 1337
+ * @s: string to be encoded
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * Return: pointer to the string
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char letter[] = "AEOTLaeotl";
+	char number[] = "4307143071";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 10; j++)
+		{
+			if (s[i] == letter[j])
+				s[i] = number[j];
+		}
+	}
+	return (s);
+}
