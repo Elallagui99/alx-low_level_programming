b0c303ac18abfe85b9e195f5cc2b6d19f7bb17c2
Elallagui99 <got.femme@gmail.com>
2023-06-30 21:34:13 +0100

    task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - print an int
+ * @n: int to print
+ * Return: nothing
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	if (n < 10)
+		_putchar(n + '0');
+	else if (n < 100)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+	else if (n < 1000)
+	{
+		_putchar(n / 100 + '0');
+		_putchar(n % 100 / 10 + '0');
+		_putchar(n % 100 % 10 + '0');
+	}
+	else if (n > 1000)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar(n % 1000 / 100 + '0');
+		_putchar(n % 1000 % 100 / 10 + '0');
+		_putchar(n % 1000 % 100 % 10 + '0');
+	}
+}
