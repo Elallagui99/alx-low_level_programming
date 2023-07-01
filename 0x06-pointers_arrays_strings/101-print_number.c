a83c754888cf403836c148f515fb7082b5589b5d
Elallagui99 <got.femme@gmail.com>
2023-07-01 01:45:39 +0100

    task 9

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,29 +8,31 @@
 
 void print_number(int n)
 {
+	unsigned int x;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		x = n * -1;
 	}
-	if (n < 10)
-		_putchar(n + '0');
-	else if (n < 100)
+	if (x < 10)
+		_putchar(x + '0');
+	else if (x < 100)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		_putchar(x / 10 + '0');
+		_putchar(x % 10 + '0');
 	}
-	else if (n < 1000)
+	else if (x < 1000)
 	{
-		_putchar(n / 100 + '0');
-		_putchar(n % 100 / 10 + '0');
-		_putchar(n % 100 % 10 + '0');
+		_putchar(x / 100 + '0');
+		_putchar(x % 100 / 10 + '0');
+		_putchar(x % 100 % 10 + '0');
 	}
-	else if (n > 1000)
+	else if (x > 1000)
 	{
-		_putchar(n / 1000 + '0');
-		_putchar(n % 1000 / 100 + '0');
-		_putchar(n % 1000 % 100 / 10 + '0');
-		_putchar(n % 1000 % 100 % 10 + '0');
+		_putchar(x / 1000 + '0');
+		_putchar(x % 1000 / 100 + '0');
+		_putchar(x % 1000 % 100 / 10 + '0');
+		_putchar(x % 1000 % 100 % 10 + '0');
 	}
 }
