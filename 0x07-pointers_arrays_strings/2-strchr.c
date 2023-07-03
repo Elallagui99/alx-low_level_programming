ea5cb1aab57d55ab017d990bc4b167137e5e6a92
Elallagui99 <got.femme@gmail.com>
2023-07-03 22:10:41 +0100

    add task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _strchr - locates a char in a string
  * @s: sting to search in
@@ -17,5 +17,5 @@ char *_strchr(char *s, char c)
 		if (s[i] == c)
 			return (s + i);
 	}
-	return (0);
+	return (NULL);
 }
