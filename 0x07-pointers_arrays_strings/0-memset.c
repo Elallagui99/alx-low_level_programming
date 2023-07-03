4c4ad5c34b70ffc85aac86906c80c3a3c96335ca
Elallagui99 <got.femme@gmail.com>
2023-07-03 20:13:51 +0100

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**_memset - filsmemory with const byte
+ * @s: pointer to string
+ * @b: const to fill with
+ * @n: n byte to fill
+ * Return: pointer to the string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
