2946142cd9c0440e53eb5c96ed7f874dd8f3e9b9
Elallagui99 <got.femme@gmail.com>
2023-07-04 01:35:16 +0100

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - sets vakue of pointer to a char
+ * @s: pointer to char
+ * @to: pointer to chr
+ * Retrn: nothing
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
