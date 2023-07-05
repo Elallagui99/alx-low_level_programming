f8d2a322787932ea3e61a791523ae9e962834eef
Elallagui99 <got.femme@gmail.com>
2023-07-05 19:51:11 +0100

    add task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to pprint
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (n != '\0')
+	{
+		_print_rev_recursion(s++);
+		_putchar(*s);
+	}
+}
