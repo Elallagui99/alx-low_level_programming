c24a727f27a08747b467b4e05a7180fc336659ed
Elallagui99 <got.femme@gmail.com>
2023-07-08 01:07:18 +0100

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name
+ * @argc: number of arguments
+ * @argc: array of arguments
+ * Return: 0 if success, 1 otherwise
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
