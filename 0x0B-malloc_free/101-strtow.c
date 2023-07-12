67accb6795d18aa2874bf5d734fc5886ee339b8e
Elallagui99 <got.femme@gmail.com>
2023-07-12 02:06:16 +0100

    task 6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,38 +2,74 @@
 #include <stdlib.h>
 
 /**
+ * _strncpy - copy n string to anoyher string
+ * @dest: string to be copied in
+ * @src: strin to be copied from
+ * @n: number of string to be copied
+ * Return: pointer to the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	for ( ; i < n; i++)
+	{
+		*(dest + i) = '\0';
+	}
+	return (dest);
+}
+
+/**
  * strtow - split string into words
  * @str: string to split
  * Return: pointer to array of words
  */
-
 char **strtow(char *str)
 {
-	int i, len = 0, l;
+	int i, len = 0, l = 0, word_count = 0;
 	char **s;
 
-	if (str == NULL)
+	if (str == NULL || *str == '\0')
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
 		len++;
 
-	s = malloc(len * sizeof(char *));
+	s = malloc((len + 1) * sizeof(char *));
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0 ; i < len; i++)
+	for (i = 0; i < len; i++)
 	{
 		if (str[i] == ' ' && l == 0)
+			continue;
+		if (str[i] == ' ')
 		{
-			*s[i] = '\n';
-			l++;
-		}
-		else if (str[i] != ' ')
-		{
-			*s[i] = str[i];
 			l = 0;
+			continue;
 		}
+		while (str[i] != ' ' && str[i] != '\0')
+		{
+			l++;
+			i++;
+		}
+		s[word_count] = malloc((l + 1) * sizeof(char));
+		if (s[word_count] == NULL)
+		{
+			for (i = 0; i < word_count; i++)
+				free(s[i]);
+			free(s);
+			return (NULL);
+		}
+		_strncpy(s[word_count], &str[i - l], l);
+		s[word_count][l] = '\0';
+		word_count++;
 	}
+	s[word_count] = NULL;
 	return (s);
 }
