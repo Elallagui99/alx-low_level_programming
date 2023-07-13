6edb15301db91eb5776f8d835e7bb3435ef02e83
Elallagui99 <got.femme@gmail.com>
2023-07-14 00:07:00 +0100

    add task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,11 +16,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	s = malloc(size * nmemb);
+	s = malloc(nmemb * size);
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size * nmemb; i++)
 		s[i] = 0;
 
 	return (s);
