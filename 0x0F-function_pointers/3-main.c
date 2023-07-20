7d2a58bc2f877e98701869a0f3d256c40ec9b3fa
Elallagui99 <got.femme@gmail.com>
2023-07-20 01:53:28 +0100

    add task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	int a, b, r;
-	int (*ptr)(int, int)
+	int (*op_f)(int, int);
 
 	if (argc != 4)
 	{
@@ -20,22 +20,18 @@ int main(int argc, char *argv[])
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-
-	if ((argv[2] == '/' || argv[2] == '%') && b == 0)
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	ptr = get_op_func(argv[2]);
-
-	if (ptr == NULL)
+	op_f = get_op_func(argv[2]);
+	if (op_f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	r = ptr(a, b);
+	r = op_f(a, b);
 	printf("%d\n", r);
 	return (0);
 }
