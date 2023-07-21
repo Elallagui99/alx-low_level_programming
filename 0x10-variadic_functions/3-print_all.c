410b21a87c542ee2a25a8197341c67cb39afe821
Elallagui99 <got.femme@gmail.com>
2023-07-21 02:29:31 +0100

    add task 4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,50 +3,54 @@
 #include <stdio.h>
 
 /**
- * print_char - print char
- * @l: variadic list
+ * print_char - prints a character
+ * @ap: va_list containing the character to print
  */
-void print_char(va_list l)
+void print_char(va_list ap)
 {
-	printf("%c", va_arg(l, int));
-}
-/**
- * print_int - print int
- * @l: variadic list
- */
-void print_int(va_list l)
-{
-	printf("%d", va_arg(l, int));
-}
-/**
- * print_float - print float
- * @l: variadic list
- */
-void print_float(va_list l)
-{
-	printf("%f", va_arg(l, double));
-}
-/**
- * print_string - print string
- * @l: variadic list
- */
-void print_string(va_list l)
-{
-	printf("%s", va_arg(l, char *));
+	printf("%c", va_arg(ap, int));
 }
 
 /**
- * print_all - prints anything
- * @format: list of types of arguments
- * passed to the function
+ * print_int - prints an integer
+ * @ap: va_list containing the integer to print
  */
+void print_int(va_list ap)
+{
+	printf("%d", va_arg(ap, int));
+}
 
+/**
+ * print_float - prints a float
+ * @ap: va_list containing the float to print
+ */
+void print_float(va_list ap)
+{
+	printf("%f", va_arg(ap, double));
+}
+
+/**
+ * print_string - prints a string
+ * @ap: va_list containing the string to print
+ */
+void print_string(va_list ap)
+{
+	char *s = va_arg(ap, char*);
+
+	printf("%s", s == NULL ? "(nil)" : s);
+}
+
+/**
+ * print_all - print any form
+ * @format: format to be printed
+ */
 void print_all(const char * const format, ...)
 {
 	va_list l;
 
 	int i = 0, j;
 	char *s = "";
+
 	print_t print[] = {
 		{"c", print_char},
 		{"i", print_int},
@@ -56,14 +60,11 @@ void print_all(const char * const format, ...)
 	};
 
 	va_start(l, format);
-
 	while (format && format[i])
 	{
 		j = 0;
 		while (j < 4 && format[i] != print[j].param[0])
-		{
-			j++;
-		}
+			++j;
 		if (j < 4)
 		{
 			printf("%s", s);
