71aea4da173639b9eb07c15dd90d46997cbf61f1
Elallagui99 <got.famme@gmail.com>
2023-08-01 23:38:04 +0100

    add task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,13 +10,17 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
+	listint_t *tmp;
 
-	while (head != NULL)
+	if (head == NULL)
+		return (NULL);
+	tmp = head;
+	while (tmp != NULL)
 	{
-		head = head->next;
+		tmp = tmp->next;
 		i++;
 		if (i == index)
-			return (head);
+			return (tmp);
 	}
 		return (NULL);
 }
