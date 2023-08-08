d676c1714be2f5a0a10cec4f2f4fe18d2b69d00b
Elallagui99 <got.famme@gmail.com>
2023-08-08 02:17:15 +0100

    add task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -38,12 +38,8 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-	{
-		close(fd);
-		return (1);
-	}
-	bytes = write(fd, text_content, len);
+	if (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
