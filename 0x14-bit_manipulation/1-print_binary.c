382f0ae83cbf5bebac4bcbead77a8e3a019b6f50
Elallagui99 <got.famme@gmail.com>
2023-10-26 21:45:46 +0100

    convert decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,10 +12,10 @@ void print_binary(unsigned long int n)
 	{
 		if (n > 1)
 			print_binary(n >> 1);/*recurtion on n/2*/
-		_putchar((n & 1) + '0');/*print only 1*/
+		_putchar((n & 1) + '0');/*print f its 1*/
 	}
 	else
 	{
-		-putchar('0');
+		_putchar('0');
 	}
 }
